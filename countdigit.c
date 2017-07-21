81e162c3e30e765455203631ccc036d42138f4cc
MSNTHIL <30283760+MSNTHIL@users.noreply.github.com>
2017-07-21 12:34:04 +0530

    countdigit.c

---
+++ countdigit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,count=0;
+printf("Enter the integer:");
+scanf("%d",&n);
+while(n!=0)
+{
+n=n/10;
+count++;
+}
+printf("Number of digits in the integer is %d",count);
+return 0;
+}
